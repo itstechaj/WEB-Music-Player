78cfff8143ae8025567d68b0351b4cc613e675a1
verma321 <92589137+verma321@users.noreply.github.com>
2022-09-16 01:59:03 +0530

    Add files via upload
    Initial Commit

---
+++ js/music-list.js
@@ -0,0 +1,70 @@
+// {
+//   name: "Here is the music name",
+//   artist: "Here is the artist name",
+//   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+// }
+
+let allMusic = [
+  {
+    name: "Ae Watan - Raazi",
+    artist: "Sunidhi Chauhan",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Teri Mitti - Kesari",
+    artist: " Arko Pravo Mukherjee, B Praak",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Aashayein - Iqbal",
+    artist: " KK, Salim–Sulaiman",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Aye Watan Aye Watan (Jalwa Jalwa) - Hindustan Ki Kasam",
+    artist: "Sukhwinder Singh",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Barsaat Ke Mausam Mein - Naajayaz",
+    artist: "K. Sanu, A. Yagnik, R. Rathod, Ila Arun",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "India Waale - Happy New Year",
+    artist: " S. Mahadevan, K.K., N. Mohan, V. Dadlani",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Jai Ho - Slumdog Millionaire",
+    artist:"A. R. Rahman",
+    img:"music-7",
+    src:"music-7"
+  },
+  {
+    name: "Chak Lein De - Chandni Chowk to China",
+    artist:"Kailash Kher",
+    img:"music-8",
+    src:"music-8"
+  },
+  {
+    name: "Thare Vaaste - Parmanu The story of Pokhran",
+    artist:"Divya Kumar",
+    img:"music-9",
+    src:"music-9"
+  },
+  {
+    name: "Zinda - Bhaag Milkha Bhaag",
+    artist:"Siddharth Mahadevan",
+    img:"music-10",
+    src:"music-10"
+  },
+  
+];
